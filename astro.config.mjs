7b0907eacbb8def22142b2df74a64fb53462024b
smzr <sammy@smzr.co.uk>
2023-01-20 23:17:37 +0000

    🎉 Inital commit

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from 'astro/config';
+import storyblok from "@storyblok/astro";
+import { loadEnv } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        storyblok({
+            accessToken: env.STORYBLOK_TOKEN,
+			components: {
+				page: 'storyblok/Page',
+				blogPost: 'storyblok/BlogPost',
+				blogPostList: 'storyblok/BlogPostList',
+				contentImage: 'storyblok/ContentImage',
+                eyebrow: 'storyblok/Eyebrow',
+			},
+        }),
+    ],
+    vite: {
+        plugins: [basicSsl()],
+        server: {
+            https: true,
+        }
+    }
+});
