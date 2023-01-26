21717458a11a2395ca74810441e107f1c08ead2c
smzr <sammy@smzr.co.uk>
2023-01-26 22:43:33 +0000

    Install netlify adaprter, update to astro 2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,28 @@ import { defineConfig } from 'astro/config';
 import storyblok from "@storyblok/astro";
 import { loadEnv } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
-
+import netlify from "@astrojs/netlify/functions";
 const env = loadEnv('', process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
+
 export default defineConfig({
-    integrations: [
-        storyblok({
-            accessToken: env.STORYBLOK_TOKEN,
-			components: {
-				page: 'storyblok/Page',
-				blogPost: 'storyblok/BlogPost',
-				blogPostList: 'storyblok/BlogPostList',
-				contentImage: 'storyblok/ContentImage',
-                eyebrow: 'storyblok/Eyebrow',
-			},
-        }),
-    ],
-    vite: {
-        plugins: [basicSsl()],
-        server: {
-            https: true,
-        }
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      page: 'storyblok/Page',
+      blogPost: 'storyblok/BlogPost',
+      blogPostList: 'storyblok/BlogPostList',
+      contentImage: 'storyblok/ContentImage',
+      eyebrow: 'storyblok/Eyebrow'
     }
+  })],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true
+    }
+  },
+  output: "server",
+  adapter: netlify()
 });
