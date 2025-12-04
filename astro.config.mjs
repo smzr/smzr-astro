f46dbea46ed7bfa5eab1c4080c4d49a0b58610f2
Sammy McKay <sammy@smzr.co.uk>
2025-12-04 18:25:25 +0000

    New landing page (#1)
    * New hero and remove storyblok
    
    * Add new hero image, booking text, and anchors
    
    * Get rid of storyblok and netlify, upgrade astro and add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,10 @@
 import { defineConfig } from 'astro/config';
-import storyblok from "@storyblok/astro";
-import { loadEnv } from 'vite';
-const env = loadEnv('', process.cwd(), 'STORYBLOK');
+
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 
 export default defineConfig({
-  integrations: [storyblok({
-    accessToken: env.STORYBLOK_TOKEN,
-    components: {
-      page: 'storyblok/Page',
-      blogPost: 'storyblok/BlogPost',
-      blogPostList: 'storyblok/BlogPostList',
-      contentImage: 'storyblok/ContentImage',
-      eyebrow: 'storyblok/Eyebrow',
-      contact: 'storyblok/Contact',
-    }
-  })]
+  site: 'https://smzr.co.uk',
+  adapter: cloudflare(),
 });
